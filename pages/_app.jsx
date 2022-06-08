bbafd6decb91ca74c15cdbf3b6b5439623726b09
igaboo <dw.jaden@gmail.com>
2022-06-08 01:53:42 -0700

    added context wrapper, toaster, and layout

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,17 @@
 import "../sass/globals.scss";
 
+import { StateContext } from "../context/StateContext";
+import { Layout } from "../components";
+
+import { Toaster } from "react-hot-toast";
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <StateContext>
+      <Toaster />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
