3c90480ce284c8c501f09c5653ea5dbb715cbb5c
igaboo <dw.jaden@gmail.com>
2022-06-08 16:32:39 -0700

    created contact page

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,5 +1,68 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./Contact.module.scss";
 
+import Link from "next/link";
+
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <header>
+        <h1>How can we help?</h1>
+        <p>Get in touch and let us know how we can help.</p>
+      </header>
+      <div className={styles.grid}>
+        <div className={styles.card}>
+          <p>Support</p>
+          <p className="dim">
+            Contact our support email for help with returns, refunds, shipping
+            info, or any other questions.
+          </p>
+          <button className="btn-small">
+            Email help@jazzycanknot.com <FontAwesomeIcon icon={faArrowRight} />
+          </button>
+        </div>
+        <div className={styles.card}>
+          <p>Business</p>
+          <p className="dim">
+            Want to work with me? Fantastic! Send all business related inquiries
+            here.
+          </p>
+          <button className="btn-small">
+            Email jazzlyn01@live.com <FontAwesomeIcon icon={faArrowRight} />
+          </button>
+        </div>
+        <div className={styles.card}>
+          <p>Legal</p>
+          <ul>
+            <li className="btn-small">
+              <Link href="/return-policy">
+                <a>Return Policy</a>
+              </Link>
+              <FontAwesomeIcon icon={faArrowRight} />
+            </li>
+            <li className="btn-small">
+              <Link href="/quality-guarantee">
+                <a>Quality Guarantee</a>
+              </Link>
+              <FontAwesomeIcon icon={faArrowRight} />
+            </li>
+            <li className="btn-small">
+              <Link href="/privacy-policy">
+                <a>Privacy Policy</a>
+              </Link>
+              <FontAwesomeIcon icon={faArrowRight} />
+            </li>
+            <li className="btn-small">
+              <Link href="/terms-of-service">
+                <a>Terms of Service</a>
+              </Link>
+              <FontAwesomeIcon icon={faArrowRight} />
+            </li>
+          </ul>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
 }
