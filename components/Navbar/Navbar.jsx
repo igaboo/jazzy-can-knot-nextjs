8231a14fac4b2a41665cb9496a470447ab0b9a29
igaboo <dw.jaden@gmail.com>
2022-06-08 01:52:04 -0700

    added navbar functionality

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -3,22 +3,48 @@ import styles from "./Navbar.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
+import Link from "next/link";
+import { useStateContext } from "../../context/StateContext";
+import Cart from "../Cart/Cart";
+
+import { useRouter } from "next/router";
+
 export default function Navbar() {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
+  const router = useRouter();
+
   return (
-    <div className={styles.bg}>
-      <div className={styles.container}>
-        <h1 className="logo">JCK</h1>
-        <h6>
-          <ul>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Blankets</li>
-            <li>
-              <FontAwesomeIcon icon={faShoppingCart} />
-            </li>
-          </ul>
-        </h6>
+    <>
+      <div className={styles.bg}>
+        <div className={styles.container}>
+          <Link href="/">
+            <a>
+              <h1 className="logo">JCK</h1>
+            </a>
+          </Link>
+          <h6>
+            <ul>
+              <li className={router.pathname === "/about" && styles.active}>
+                <Link href="/about">About</Link>
+              </li>
+              <li className={router.pathname === "/contact" && styles.active}>
+                <Link href="/contact">Contact</Link>
+              </li>
+              <li className={router.pathname === "/products" && styles.active}>
+                <Link href="/products">Products</Link>
+              </li>
+              <li className={styles.cart}>
+                <button onClick={() => setShowCart(true)}>
+                  <FontAwesomeIcon icon={faShoppingCart} />
+                  <span>{totalQuantities}</span>
+                </button>
+              </li>
+            </ul>
+          </h6>
+        </div>
+        <Cart showCart={showCart} />
       </div>
-    </div>
+    </>
   );
 }
