2febf1d41dcabc1a4d22dc7e689bf455da247868
igaboo <dw.jaden@gmail.com>
2022-06-08 01:49:53 -0700

    created bulk contact section

--- components/BulkContact/BulkContact.jsx
+++ components/BulkContact/BulkContact.jsx
@@ -1,3 +1,17 @@
+import styles from "./BulkContact.module.scss";
+
+import Button from "../Button/Button";
+
 export default function BulkContact() {
-  return <div>BulkContact</div>;
+  return (
+    <div className={styles.container}>
+      <h2>Looking to buy in bulk?</h2>
+      <p>
+        Gifting the entire family tie blankets? What a lucky bunch! Contact me
+        ahead of time to ensure availability, as well as to discuss pricing, and
+        turn around times.
+      </p>
+      <Button text="Contact Me" href="/contact" color="#F9FFF6" />
+    </div>
+  );
 }
