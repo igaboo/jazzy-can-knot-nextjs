59d1a060aaf3eab4eb0f4b73bdaa8efa86bd3b59
igaboo <dw.jaden@gmail.com>
2022-06-08 01:55:30 -0700

    created products page

---
+++ pages/products/index.jsx
@@ -0,0 +1,27 @@
+import styles from "./Products.module.scss";
+
+import { Products as ProductsList } from "../../components";
+
+import { client } from "../../lib/client";
+
+export default function Products({ products }) {
+  return (
+    <>
+      <div className={styles.container}>
+        <h1>Tie Blankets</h1>
+        <h5 className="dim">Showing {products.length} blankets</h5>
+      </div>
+      <ProductsList products={products} />
+      <div className={styles.gap} />
+    </>
+  );
+}
+
+export const getStaticProps = async () => {
+  const productsQuery = `*[_type == "product"]`;
+  const products = await client.fetch(productsQuery);
+
+  return {
+    props: { products },
+  };
+};
