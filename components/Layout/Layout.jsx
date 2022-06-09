d13c3c87bae48ffd4b6e9e2383b93895ed68a74c
igaboo <dw.jaden@gmail.com>
2022-06-09 16:51:50 -0700

    added dynamic theme color meta tag

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -3,15 +3,18 @@ import NotificationBanner from "../NotificationBanner/NotificationBanner";
 
 import { useRouter } from "next/router";
 import Head from "next/head";
+import { useStateContext } from "../../context/StateContext";
 
 export default function Layout({ children }) {
   const router = useRouter();
+
+  const { navColor } = useStateContext();
 
   return (
     <div className="test">
       <Head>
         <title>Jazzy Can Knot</title>
-        <meta name="theme-color" content="#fff" />
+        <meta name="theme-color" content={navColor} />
       </Head>
 
       {router.pathname === "/" && <NotificationBanner />}
