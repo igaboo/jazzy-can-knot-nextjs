3e8bc21209913ddc87d981a55f07d2e3fb3e4309
igaboo <dw.jaden@gmail.com>
2022-06-09 16:49:23 -0700

    change margins, added footer color reset

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -3,8 +3,16 @@ import styles from "./Products.module.scss";
 import { Products as ProductsList } from "../../components";
 
 import { client } from "../../lib/client";
+import { useStateContext } from "../../context/StateContext";
+import { useEffect } from "react";
 
 export default function Products({ products }) {
+  const { setFooterColor } = useStateContext();
+
+  useEffect(() => {
+    setFooterColor();
+  }, []);
+
   return (
     <>
       <div className={styles.container}>
