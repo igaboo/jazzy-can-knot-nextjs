080980f7082316f5c7007a4b2f2ac002862ae3d9
igaboo <dw.jaden@gmail.com>
2022-06-09 19:09:24 -0700

    created animations

---
+++ animation/FadeIn.jsx
@@ -0,0 +1,21 @@
+import { motion } from "framer-motion";
+
+export default function FadeIn({ children, each }) {
+  const animation = {
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+  };
+
+  const viewport = { once: true };
+
+  return (
+    <motion.div
+      style={{ width: "100%", height: "100%" }}
+      initial={animation.from}
+      whileInView={animation.to}
+      viewport={viewport}
+    >
+      {children}
+    </motion.div>
+  );
+}
---
+++ animation/SlideUp.jsx
@@ -0,0 +1,27 @@
+import { motion } from "framer-motion";
+
+export default function SlideUp({ children, delay }) {
+  const animation = {
+    from: { y: 50, opacity: 0 },
+    to: { y: 0, opacity: 1 },
+  };
+
+  const viewport = { once: true };
+
+  return (
+    <motion.div
+      style={{ width: "100%" }}
+      initial={animation.from}
+      whileInView={animation.to}
+      viewport={viewport}
+      transition={{
+        type: "spring",
+        duration: 1,
+        bounce: 0.3,
+        delay: delay && delay,
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
