b280c457d50e15bdd2941faa63bed4ec7f25977f
igaboo <dw.jaden@gmail.com>
2022-06-16 01:07:47 -0700

    removed animations, tidied up imports

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,38 +1,32 @@
 import styles from "./About.module.scss";
 
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import Link from "next/link";
 import Image from "next/image";
-import SlideUp from "../../animation/SlideUp";
-import FadeIn from "../../animation/FadeIn";
+
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function About() {
   return (
     <div className={styles.container}>
-      <FadeIn>
-        <div className={styles.column}>
-          <Image src="/images/about.jpg" width={597} height={448} />
-        </div>
-      </FadeIn>
+      <div className={styles.column}>
+        <Image src="/images/about.jpg" width={597} height={448} />
+      </div>
 
-      <FadeIn>
-        <div className={styles.column}>
-          <h1>Hi, I&apos;m Jazzy! 👋</h1>
-          <p>
-            Here you should write a short about section, detailing who you are
-            and why you&apos;re making and selling blankets - something along
-            those lines. It can take up as much room as it needs, the section
-            will accommodate for whatever size, but I&apos;m expecting it to be
-            around 5 or 6 lines.
-          </p>
-          <p>
-            Follow me on <FontAwesomeIcon icon={faInstagram} />
-            <Link href="https://www.instagram.com/jazzycanknot">Instagram</Link>
-          </p>
-        </div>
-      </FadeIn>
+      <div className={styles.column}>
+        <h1>Hi, I&apos;m Jazzy! 👋</h1>
+        <p>
+          Here you should write a short about section, detailing who you are and
+          why you&apos;re making and selling blankets - something along those
+          lines. It can take up as much room as it needs, the section will
+          accommodate for whatever size, but I&apos;m expecting it to be around
+          5 or 6 lines.
+        </p>
+        <p>
+          Follow me on <FontAwesomeIcon icon={faInstagram} />
+          <Link href="https://www.instagram.com/jazzycanknot">Instagram</Link>
+        </p>
+      </div>
     </div>
   );
 }
