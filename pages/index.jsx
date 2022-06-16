2e66ff98b306b3c9c98705e9a2d7775aa8612672
igaboo <dw.jaden@gmail.com>
2022-06-16 01:07:29 -0700

    updates

--- pages/index.jsx
+++ pages/index.jsx
@@ -40,7 +40,6 @@ export default function Index({ products, banner, reviews }) {
           reviews={reviews && reviews}
         />
       </div>
-
       <BulkContact />
       <FeaturedPosts />
     </>
